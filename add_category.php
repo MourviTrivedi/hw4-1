78d210047b2bcc677a02a898a51182e74b9a7256
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-02-27 15:48:51 -0500

    file update

--- add_category.php
+++ add_category.php
@@ -8,14 +8,14 @@ include('error.php');
 else {
 require_once('database.php');
 
-$query = 'inster into categories_guitar1
+$query = 'INSERT INTO categories_guitar1
 (categoryName)
 VALUES
 (:name)';
 
 $statement = $db->prepare($query);
 $statement = bindValue(':name', $name);
-$statement-> execute();
+$statement->execute();
 $statement->closeCursor();
 
 include('index.php');
